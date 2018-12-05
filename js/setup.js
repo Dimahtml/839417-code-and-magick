6655a8d5b6dd2b0f36cf4ea850b0c425111f5fc3
Dimahtml <bomabo@mail.ru>
2018-12-05 17:02:17 +0500

    Все пункты задания выполнены

--- js/setup.js
+++ js/setup.js
@@ -8,6 +8,7 @@ var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Крис�
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 // Возвращает случайное целое число между min (включительно) и max (не включая max)
 function getRandom(min, max) {
@@ -60,18 +61,14 @@ var ENTER_KEYCODE = 13;
 // кнопки открыть и закрыть попап
 var buttonSetupOpen = document.querySelector('.setup-open');
 var buttonSetupClose = setup.querySelector('.setup-close');
+// поле ввода имени персонажа
 var userNameInput = document.querySelector('.setup-user-name');
-
 var isFocused = 0;
-
 userNameInput.addEventListener('focus', function() {
   isFocused = 1;
-  console.log(isFocused);
 });
-
 userNameInput.addEventListener('blur', function() {
   isFocused = 0;
-  console.log(isFocused);
 });
 
 var onPopupEscPress = function (evt) {
@@ -108,5 +105,54 @@ buttonSetupClose.addEventListener('click', function () {
 buttonSetupClose.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
     closePopup ();
+  }
+});
+
+// функция для изменения цвета плаща или глаз (В РАЗРАБОТКЕ. нужна ли она вообще?)
+// var changeColor = function (classOfElement, arrayOfData) {
+//   var wizardElement = document.querySelector('.setup-wizard').querySelector('classOfElement');
+//   var colorIndex = 1;
+//   wizardElement.addEventListener('click', function () {
+//     wizardElement.style.fill = arrayOfData[colorIndex];
+//     colorIndex++;
+//     if (colorIndex === arrayOfData.length) {
+//       colorIndex = 0;
+//     };
+//   });
+// };
+// changeColor ('.wizard-coat', COAT_COLORS);
+
+// меняем цвет плаща волшебника
+var wizardCoat = document.querySelector('.setup-wizard').querySelector('.wizard-coat');
+var colorCoatIndex = 1;
+wizardCoat.addEventListener('click', function () {
+  wizardCoat.style.fill = COAT_COLORS[colorCoatIndex];
+  colorCoatIndex++;
+  if (colorCoatIndex === COAT_COLORS.length) {
+    colorCoatIndex = 0;
+  }
+});
+// меняем цвет глаз
+var wizardEyes = document.querySelector('.setup-wizard').querySelector('.wizard-eyes');
+var colorEyesIndex = 1;
+wizardEyes.addEventListener('click', function () {
+  wizardEyes.style.fill = EYES_COLORS[colorEyesIndex];
+  colorEyesIndex++;
+  if (colorEyesIndex === EYES_COLORS.length) {
+    colorEyesIndex = 0;
+  }
+});
+// Изменение цвета фаерболов по нажатию. Изменение фона у блока .setup-fireball-wrap
+var wizardFireball = document.querySelector('.setup-fireball-wrap');
+// чтобы на сервер отправились правильные данные, при изменении параметров персонажа
+// должно изменяться и значение соответствующего скрытого инпута.
+var wizardFireballInput = document.querySelector('.setup-fireball-wrap').querySelector('input');
+var colorFireballIndex = 1;
+wizardFireball.addEventListener('click', function () {
+  wizardFireball.style.background = FIREBALL_COLORS[colorFireballIndex];
+  wizardFireballInput.value = FIREBALL_COLORS[colorFireballIndex];
+  colorFireballIndex++;
+  if (colorFireballIndex === FIREBALL_COLORS.length) {
+    colorFireballIndex = 0;
   }
 });
